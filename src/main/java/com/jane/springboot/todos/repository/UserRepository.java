58a96cc2956ffda675b797d5cec4e53c2b9acc93
Ritu Bafna <ritujane78@gmail.com>
2025-09-02 18:51:08 +0545

    add user repository

---
+++ src/main/java/com/jane/springboot/todos/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.jane.springboot.todos.repository;
+
+import com.jane.springboot.todos.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
